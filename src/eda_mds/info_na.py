89a54b2ad6f38e37669b78be3e1c719aa3acfa52
Simon Frew <simon.r.frew@gmail.com>
2024-01-17 22:29:34 -0800

    dev: column and row info generation

--- src/eda_mds/info_na.py
+++ src/eda_mds/info_na.py
@@ -1,5 +1,7 @@
 import numpy as np
 import pandas as pd
+import warnings
+
 
 def info_na(df):
     """
@@ -12,7 +14,8 @@ def info_na(df):
     - Columns:
         - Index
         - Name
-        - Non-Null Count
+        - Null Count
+        - Null Percentage
         - Dtype
     - Rows:
         - Count of rows with any Null Values.
@@ -29,9 +32,39 @@ def info_na(df):
 
     if not isinstance(df, pd.DataFrame):
         raise TypeError("Input `df` must be a Pandas DataFrame")
-    
-    if pd.isna(df).all(axis=None): 
-        raise Warning("Input `df` contains all NA values")
 
-    
+    if pd.isna(df).all(axis=None):
+        warnings.warn("Input `df` contains all NA values")
+
     print("info_na()")
+
+    column_info = pd.DataFrame(
+        {
+            "#": np.arange(df.shape[1]),
+            "Column": df.columns.values,
+            "Null Count": df.isna().sum(axis=0),
+            "Null %": (df.isna().sum(axis=0) / df.shape[0] * 100).round(2),
+            "dtype": df.dtypes,
+        }
+    )
+
+    row_info = pd.Series(
+        {
+            "Total Rows": df.shape[0],
+            "Any Null Count": df.isna().any(axis=1).sum(),
+            "Any Null %": (df.isna().any(axis=1).sum() / df.shape[0] * 100).round(2),
+            "All Null Count": df.isna().all(axis=1).sum(),
+            "All Null %": (df.isna().all(axis=1).sum() / df.shape[0] * 100).round(2),
+            "Mean Null Count / Row": (
+                df.isna().sum(axis=1).drop(df.index[df.isna().sum(axis=1) == 0]).mean()
+            ),
+            "Mean Null % / Row": (
+                df.isna().sum(axis=1).drop(df.index[df.isna().sum(axis=1) == 0]).mean()
+                / (df.isna().sum(axis=1) != 0).sum()
+                * 100
+            ).round(2),
+        }
+    )
+
+    print(column_info)
+    print(row_info)
