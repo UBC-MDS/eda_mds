1fab346c5402706c082366d0905b853ed7d5db6b
Simon Frew <simon.r.frew@gmail.com>
2024-01-19 14:53:49 -0800

    dev: output formatting

--- src/eda_mds/info_na.py
+++ src/eda_mds/info_na.py
@@ -11,6 +11,7 @@ def info_na(df):
     This function prints the following information about a DataFrame:
     - DataFrame Type
     - Shape:
+    - Memory Usage
     - Columns:
         - Index
         - Name
@@ -27,7 +28,6 @@ def info_na(df):
         - Std.Dev Null Count: Standard devation of the number of Null Values per row. 
         - Max Null Count: The maximum number of Null Values found in a row.
         - Min Null Count: The minimum number of Null Values found in a row. 
-    - Memory Usage
 
     Parameters
     ----------
@@ -49,24 +49,24 @@ def info_na(df):
     column_info = pd.DataFrame(
         {
             "#": np.arange(df.shape[1]),
-            "Column": df.columns.values,
-            "Null Count": df.isna().sum(axis=0),
-            "Null %": (df.isna().sum(axis=0) / df.shape[0] * 100).round(2),
+            "column": df.columns.values,
+            "null count": df.isna().sum(axis=0),
+            "null %": (df.isna().sum(axis=0) / df.shape[0] * 100).round(2),
             "dtype": df.dtypes,
         }
     )
 
     row_info = pd.Series(
         {
-            "Total Rows": df.shape[0],
-            "Any Null Count": df.isna().any(axis=1).sum(),
-            "Any Null %": (df.isna().any(axis=1).sum() / df.shape[0] * 100).round(2),
-            "All Null Count": df.isna().all(axis=1).sum(),
-            "All Null %": (df.isna().all(axis=1).sum() / df.shape[0] * 100).round(2),
-            "Mean Null Count": df.isna().sum(axis=1).mean().round(2),
-            "Std.Dev Null Count": df.isna().sum(axis=1).std().round(2),
-            "Max Null Count": df.isna().sum(axis=1).max(),
-            "Min Null Count": df.isna().sum(axis=1).min()
+            "total rows": df.shape[0],
+            "any null count": df.isna().any(axis=1).sum(),
+            "any null %": (df.isna().any(axis=1).sum() / df.shape[0] * 100).round(2),
+            "all null count": df.isna().all(axis=1).sum(),
+            "all null %": (df.isna().all(axis=1).sum() / df.shape[0] * 100).round(2),
+            "mean null count": df.isna().sum(axis=1).mean().round(2),
+            "std.dev null count": df.isna().sum(axis=1).std().round(2),
+            "max null count": df.isna().sum(axis=1).max(),
+            "min null count": df.isna().sum(axis=1).min()
         }
     )
 
@@ -78,11 +78,26 @@ def info_na(df):
         n += 1
     memory_info = f"{np.round(memory_bytes, 1)} {suffix[n]}"
 
-    print(type_info)
-    print(shape_info)
-    print(column_info)
-    print(row_info)
-    print(memory_info)
+    # print(type_info)
+    # print(shape_info)
+    # print(column_info)
+    # print(row_info)
+    # print(memory_info)
+
+
+    output = f"""
+type: {type_info}
+shape: {shape_info}
+memory usage: {memory_info}
+--------
+columns:
+{column_info.to_string(index=False)}
+-----
+rows:
+{row_info.to_string()}
+"""
+    
+    print(output)
 
 # Tasks: 
 # - Dataframe class
